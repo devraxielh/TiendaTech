bf73bae43a300fac4101e78fda9f1217f5c563c1
Rodrigo Garcia <dev.raxielh@gmail.com>
2025-04-23 09:44:13 -0500

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+const URL = "https://teachablemachine.withgoogle.com/models/F7h51i7un/";
+let model, webcam, labelContainer, maxPredictions;
+
+async function init() {
+    console.log("Loading model...");
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+async function predict() {
+    const prediction = await model.predict(webcam.canvas);
+    console.log(prediction);
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+}
