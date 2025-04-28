1291ed35f2fa8be072e3833f7ddd3bde33785c29
Rodrigo Garcia <dev.raxielh@gmail.com>
2025-04-28 11:34:52 -0500

    init

---
+++ componentes/cards.php
@@ -0,0 +1,34 @@
+<div class="row">
+          <div class="col">
+            <div class="card">
+                <img src="https://cdn.dlcompare.com/others_jpg/upload/news/image/se-acaban-de-filtrar-las-especif-68bd2905-image-4902328ec.jpg.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card">
+                <img src="https://cdn.dlcompare.com/others_jpg/upload/news/image/se-acaban-de-filtrar-las-especif-68bd2905-image-4902328ec.jpg.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card">
+                <img src="https://cdn.dlcompare.com/others_jpg/upload/news/image/se-acaban-de-filtrar-las-especif-68bd2905-image-4902328ec.jpg.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card">
+                <img src="https://cdn.dlcompare.com/others_jpg/upload/news/image/se-acaban-de-filtrar-las-especif-68bd2905-image-4902328ec.jpg.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+          </div>
+        </div>
